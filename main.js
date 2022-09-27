412931da3fec19e395f28966ac4a8a1cb6ce7b95
Victoria0292 <tlynn0292@gmail.com>
2022-09-27 10:29:15 -0400

    add userQuestion, console.log, randomNumber and eightBall main.js

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ let userName = "Vikki";
 /*Ternary expression is a condition ? expression to execute if the condition is 
 truthy : expression to execute if the condition is falsy*/
 userName ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
+
+const userQuestion = 'Are ghost real?';
+
+//console.log() using string interpolation
+console.log(`${userName} has asked - ${userQuestion}`);
+
+//Math.random() returns a value between 0 and 1 Math.floor() rounding down to the nearest whole number
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
