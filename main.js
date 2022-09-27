7385b8614d92a06de124d62b501ea644b437a434
Victoria0292 <tlynn0292@gmail.com>
2022-09-27 10:16:27 -0400

    userName and ternary expression main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+let userName = "Vikki";
+/*Ternary expression is a condition ? expression to execute if the condition is 
+truthy : expression to execute if the condition is falsy*/
+userName ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
